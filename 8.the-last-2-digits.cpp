04f7ed78018e13770fda5716ad0dfcfea883a7e5
Konstantin Terziev <terzievk@yahoo.com>
2026-02-27 18:28:09 +0200

    fix the last 2 digits

--- 8.the-last-2-digits.cpp
+++ 8.the-last-2-digits.cpp
@@ -3,12 +3,13 @@
 // https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/Y
 
 #include <iostream>
+#include <cstdint>
 
 int main() {
-  int64_t a;
-  int64_t b;
-  int64_t c;
-  int64_t d;
+  uint64_t a;
+  uint64_t b;
+  uint64_t c;
+  uint64_t d;
 
   std::cin >> a >> b >> c >> d;
 
@@ -17,7 +18,21 @@ int main() {
   c %= 100;
   d %= 100;
 
-  std::cout << (a * b * c * d) % 100 << '\n';
 
+  uint64_t r{(a * b * c * d) % 100};
+
+
+  if (r == 0) {
+    // since a b c and d are all >= 2
+    // if the result is 0 the last two digits are 00
+    std::cout << "00\n";
+  } else if (r < 10) {
+    // since a b c and d are all >= 2
+    // result can't be less than 10
+    // so always add 0 if it's single digit
+    std::cout << '0' << r << '\n';
+  } else {
+    std::cout << r << '\n';
+  }
   return 0;
 }

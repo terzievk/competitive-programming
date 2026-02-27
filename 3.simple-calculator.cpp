3ce4149e3ae0325bbbf6302b60789a781c954ce5
Konstantin Terziev <terzievk@yahoo.com>
2026-02-27 16:24:15 +0200

    simple calculator

---
+++ 3.simple-calculator.cpp
@@ -0,0 +1,17 @@
+// copyright
+// Source:
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/C
+
+#include <iostream>
+
+int main() {
+  int64_t x;
+  int64_t y;
+
+  std::cin >> x >> y;
+  std::cout
+      << x << " + " << y << " = " << x + y << '\n'
+      << x << " * " << y << " = " << x * y << '\n'
+      << x << " - " << y << " = " << x - y << '\n';
+  return 0;
+}

622584c46d0e5bf7927d9d7c81dab0e343e87f91
Konstantin Terziev <terzievk@yahoo.com>
2026-02-27 16:35:05 +0200

    difference

---
+++ 4.difference.cpp
@@ -0,0 +1,17 @@
+// copyright
+// Source:
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D
+
+#include <iostream>
+
+int main() {
+  int64_t a;
+  int64_t b;
+  int64_t c;
+  int64_t d;
+
+  std::cin >> a >> b >> c >> d;
+
+  std::cout << "Difference = " << a * b - c * d << '\n';
+  return 0;
+}

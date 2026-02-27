69e02ce9b8d56a164302088ec247fee69ad8efa8
Konstantin Terziev <terzievk@yahoo.com>
2026-02-27 16:55:47 +0200

    summation from 1 to N

---
+++ 7.summation-from-1-to-n.cpp
@@ -0,0 +1,14 @@
+// copyright
+// Source:
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/G
+
+#include <iostream>
+
+int main() {
+  int64_t x;
+  std::cin >> x;
+
+  std::cout << x * (x+1) / 2 << '\n';
+
+  return 0;
+}

219d6ba088b63e778b43d9df14f2a9470a7e872c
Konstantin Terziev <terzievk@yahoo.com>
2026-02-27 16:51:25 +0200

    age in days

---
+++ 6.age-in-days.cpp
@@ -0,0 +1,20 @@
+// copyright
+// Source:
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/R
+
+#include <iostream>
+
+int main() {
+  int d;
+
+  std::cin >> d;
+
+  std::cout << d / 365 << " years\n";
+  d %= 365;
+  std::cout << d / 30 << " months\n";
+  d %= 30;
+  std::cout << d << " days\n";
+
+
+  return 0;
+}

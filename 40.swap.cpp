28bb96976c4e3549a813463ab891fdfda99e7312
Konstantin Terziev <terzievk@yahoo.com>
2026-03-01 16:47:43 +0200

    swap

---
+++ 40.swap.cpp
@@ -0,0 +1,19 @@
+/* copyright
+   Source:
+
+*/
+#include <bits/stdc++.h>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  int x, y;
+  std::cin >> x >> y;
+
+  std::swap(x, y);
+
+  std::cout << x << ' ' << y << '\n';
+
+  return 0;
+}

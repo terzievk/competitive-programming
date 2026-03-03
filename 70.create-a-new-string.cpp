a2d9769aaa40700ee840f9b5c192ddeb27373cc3
Konstantin Terziev <terzievk@yahoo.com>
2026-03-03 16:48:35 +0200

    70, 71 & 72

---
+++ 70.create-a-new-string.cpp
@@ -0,0 +1,16 @@
+/* copyright
+   Source:
+   https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/A
+*/
+#include <bits/stdc++.h>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  std::string s, t;
+  std::cin >> s >> t;
+  std::cout << s.size() << ' ' << t.size() << '\n';
+  std::cout << s << ' ' << t << '\n';
+  return 0;
+}
---
+++ 71.lets-use-getline.cpp
@@ -0,0 +1,18 @@
+/* copyright
+   Source:
+   https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/B
+*/
+#include <bits/stdc++.h>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  char s[1000*1000];
+
+  std::cin.getline(s, 1000*1000, '\\');
+
+  std::cout << s << '\n';
+
+  return 0;
+}

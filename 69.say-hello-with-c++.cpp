4133b6d7fda48580ebfb0355a66fe3299c08c2b7
Konstantin Terziev <terzievk@yahoo.com>
2026-03-03 16:35:34 +0200

    69

---
+++ 69.say-hello-with-c++.cpp
@@ -0,0 +1,17 @@
+/* copyright
+   Source:
+   https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/A
+*/
+#include <bits/stdc++.h>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  std::string s;
+  std::cin >> s;
+
+  std::cout << "Hello, " << s << '\n';
+
+  return 0;
+}

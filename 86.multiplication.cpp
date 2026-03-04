b8e24add3f96f399deed267a1a65542df3935770
Konstantin Terziev <terzievk@yahoo.com>
2026-03-04 17:44:03 +0200

    86

---
+++ 86.multiplication.cpp
@@ -0,0 +1,16 @@
+/* copyright
+   Source:
+   https://vjudge.net/problem/AtCoder-abc169_a
+*/
+#include <bits/stdc++.h>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  int a, b;
+  std::cin >> a >> b;
+  std::cout << a * b << '\n';
+
+  return 0;
+}

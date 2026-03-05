3a455d36ca5f66d0b7e8459ba4b7a22a5d41f6e8
Konstantin Terziev <terzievk@yahoo.com>
2026-03-05 11:55:20 +0200

    91 & 92

---
+++ 92.biscuit-generator.cpp
@@ -0,0 +1,16 @@
+/* copyright
+   Source:
+   https://vjudge.net/problem/AtCoder-abc125_a
+*/
+#include <bits/stdc++.h>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  int a, b, t;
+  std::cin >> a >> b >> t;
+
+  std::cout << t/a*b << '\n';
+  return 0;
+}

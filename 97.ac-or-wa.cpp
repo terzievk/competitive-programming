c8c4dbfe7bcd3e790aeb3e1624c3a83cc8d5abef
Konstantin Terziev <terzievk@yahoo.com>
2026-03-05 12:16:57 +0200

    97

---
+++ 97.ac-or-wa.cpp
@@ -0,0 +1,16 @@
+/* copyright
+   Source:
+   https://vjudge.net/problem/AtCoder-abc152_a
+*/
+#include <bits/stdc++.h>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  int n, m;
+  std::cin >> n >> m;
+
+  std::cout << (n == m ? "Yes" : "No") << '\n';
+  return 0;
+}

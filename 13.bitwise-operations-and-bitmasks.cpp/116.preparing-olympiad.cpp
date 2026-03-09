395fc9c3bf456132a7f44b45e716f60dc8c22d4a
Konstantin Terziev <terzievk@yahoo.com>
2026-03-09 19:09:26 +0200

    116

---
+++ 13.bitwise-operations-and-bitmasks.cpp/116.preparing-olympiad.cpp
@@ -0,0 +1,48 @@
+/* copyright
+   Source:
+   https://codeforces.com/problemset/problem/550/B
+ */
+#include <bits/stdc++.h>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  int n;
+  uint64_t l, r, x;
+  std::cin >> n >> l >> r >> x;
+
+  uint64_t c[15];
+
+  for (int i{}; i < n; ++i) {
+    std::cin >> c[i];
+  }
+
+  int count{};
+  for (uint64_t i{}; i < (1ull << (n + 1)); ++i) {
+    uint64_t sum{};
+    uint64_t min{1000*1000*1000};
+    uint64_t max{0};
+    for (int j{}; j < n; ++j) {
+      if (i & (1 << j)) {
+        sum += c[j];
+
+        if (c[j] < min) {
+          min = c[j];
+        }
+
+        if (c[j] > max) {
+          max = c[j];
+        }
+      }
+    }
+
+    if (sum >= l && sum <= r && (max - min >= x)) {
+      ++count;
+    }
+  }
+
+  std::cout << count/2 << '\n';
+
+  return 0;
+}

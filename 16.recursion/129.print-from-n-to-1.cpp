e7e1f2dcd30b6b51480a09821a499e45ddb870f9
Konstantin Terziev <terzievk@yahoo.com>
2026-03-11 13:41:47 +0200

    128 & 129

---
+++ 16.recursion/129.print-from-n-to-1.cpp
@@ -0,0 +1,27 @@
+/* copyright
+   Source:
+   https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/C
+ */
+#include <bits/stdc++.h>
+
+void print(int n) {
+  if (!n) {
+    return;
+  }
+
+  std::cout << n << '\n';
+
+  print(n - 1);
+
+  return;
+}
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+  int n;
+  std::cin >> n;
+
+  print(n);
+  return 0;
+}
